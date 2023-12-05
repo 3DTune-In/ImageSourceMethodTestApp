1307874bb05f943bd01e2d59619fe93079fe64ae
FabArr <rfarrebola@uma.es>
2023-12-05 09:06:31 +0100

    The adjustment of the absorption coefficients can be done with the average (of energies) of the L and R channels

---
+++ ISM_OSC_Tester/MatlabOscTester/ViewResulsAbsorbAdjustAv.m
@@ -0,0 +1,287 @@
+%% This script shows the intermediate and final graphical results 
+%% derived from the process of adjusting the absorption values. 
+%% The energy factor for the hybrid method is also obtained.
+%% (this script replaces EnergyFactor_9Bands_Visual_Fit.m)
+
+% Author: Fabian Arrebola (17/10/2023) 
+% contact: areyes@uma.es
+% 3DDIANA research group. University of Malaga
+% Project: SONICOM
+% 
+% Copyright (C) 2023 Universidad de Málaga
+
+%% As input parameters are taken:
+%% a) Impulse response obtained by the ISM with a sufficiently high 
+%%    reflection order 
+%% b) BRIR of the room to be simulated (BRIR.wav IR obtained by 
+%%    convolution and windowing with a pruning distance of 1 meter).
+%% N = DpMax-DpMin+1;
+%% DpMin = Initial pruning distance
+%% DpMax = Final pruning distance
+%% DpMinFit = first distance value to carry out the process of fitting the 
+%% slopes of the energy factors
+
+%% Output
+%  'EnergyFactor.mat'     <--  'FactorMeanValue'
+
+%% PRUNING DISTANCES &  Configuration parameters for ISM 
+cd 'C:\Repos\of_v0.11.2_vs2017_release\ImageSourceMethodTestApp\bin\data\resources\SeriesIr';
+% cd 'C:\Repos\HIBRIDO PRUEBAS\New LAB 28 2 20';
+load ("DistanceRange.mat");
+load ("ParamsISM.mat")
+
+x=[DpMin:1:DpMax];               % Initial and final pruning distance
+
+L=1; R=2;                        % Channel
+
+%% Folder with impulse responses
+cd 'C:\Repos\of_v0.11.2_vs2017_release\ImageSourceMethodTestApp\bin\data\resources\SeriesIr\12';
+% cd 'C:\Repos\HIBRIDO PRUEBAS\New LAB 28 2 20\0'
+% cd 'C:\Repos\HIBRIDO PRUEBAS\New LAB 32 2 20\12'
+load ("FiInfAbsorb.mat");
+load ("FiInfSlopes.mat");
+
+
+%%   BANDS
+%    62,5    125     250      500      1000       2000       4000       8000       16000
+% B =[44 88; 89 176; 177 353; 354 707; 708 1414; 1415 2828; 2829 5657; 5658 11314; 11315 22016;];
+
+%%   9 BANDS
+NB=9;
+B =[44 88; 89 176; 177 353; 354 707; 708 1414; 1415 2828; 2829 5657; 5658 11314; 11315 22016;];
+Blo=[ 1    89      177      354      708       1415       2829       5658        11315       ];
+Bhi=[  88     176      353      707      1414       2828       5657       11314        22016 ];
+
+%% Number of Impulse Responses
+NumIRs = DpMax-DpMin+1;
+
+%% Read file with BRIR
+BRIRFile=dir(['BRIR*.wav']);  %BRIR obtained with a pruning distance of 1 meter
+AudioFile=BRIRFile.name;
+[t_BRIR,Fs] = audioread(AudioFile);
+
+%% Read file with ISM
+ISMFile=dir(['ISM*.wav']);      %ISM obtained with a pruning max distance 
+AudioFile=ISMFile.name;
+[t_ISM,Fs] = audioread(AudioFile);
+
+%% BRIR Energy
+%%%%%%%%%%%%%%%%
+e_BRIR= calculateEnergy(t_BRIR);
+%%%%%%% PARSEVAL RELATION --> e_BRIR (in time) == E_BRIR (in frec)
+E_BRIR= calculateEnergyFrec(Fs, t_BRIR)/length(t_BRIR); 
+%% Only one channel 
+% eBRIR_L= e_BRIR(L); eBRIR_R= e_BRIR(R);
+%% Average of both channels
+eBRIR_L = (e_BRIR(L)+e_BRIR(R))./2;
+%% --------------
+%% Total Energy in time domain
+e_TotalIsm=zeros(NumIRs,2);              
+e_TotalWin=zeros(NumIRs,2);   
+e_Total=zeros(NumIRs,2);
+%% Energy per band in frequency domain
+E_BandIsm =zeros(NB,NumIRs,2);            
+E_BandWin=zeros(NB,NumIRs,2); 
+E_BandBrir_Win=zeros(NB,NumIRs,2);        %BRIR-Win
+
+%% Calculate total and partial energies
+maxDistSL = DpMin;
+for i=1:NumIRs
+     %%  Ism IRs -------------------------------------
+     ir_Ism =  windowingISM_RIR (Fs, t_ISM, maxDistSL, 2, 1);
+     e= calculateEnergy(ir_Ism);
+     e_TotalIsm(i,:)= e;
+     % PARSEVAL RELATION --> e_TotalIsm (in time) == E_TotalIsm (in frec) 
+     E_TotalIsm= calculateEnergyFrec(Fs, ir_Ism)/length(ir_Ism); 
+     E_TotalIsm2= calculateEnergyBand(Fs, ir_Ism, Blo(1), Bhi(NB))/length(ir_Ism);                            
+     %eSumBandsI=zeros(1,2);  
+     eSumBandsI=0; %checksum
+     for j=1:NB
+        e = calculateEnergyBand(Fs, ir_Ism, Blo(j), Bhi(j)) / length(ir_Ism);  %(Bhi(j)-Blo(j)+1);  
+        E_BandIsm(j,i,:) = e;
+        eSumBandsI = eSumBandsI+E_BandIsm(j,i,:);
+     end
+     eSumBandsI= squeeze(eSumBandsI);
+     %%  Windowed IRs -------------------------------
+     ir_Win =  windowingISM_RIR (Fs, t_BRIR, maxDistSL, 2, 0);
+     e = calculateEnergy(ir_Win);
+     e_TotalWin(i,:)= e;
+     %% PARSEVAL RELATION --> e_Totalwin (in time) == E_TotalWin (in frec) 
+     E_TotalWin= calculateEnergyFrec(Fs, ir_Win)/length(ir_Win); 
+     E_TotalWin2= calculateEnergyBand(Fs, ir_Win, Blo(1), Bhi(NB))/length(ir_Ism); %(Bhi(NB)-Blo(1)+1); 
+     %eSumBandsW=zeros(1,2); %checksum
+     eSumBandsW=0; %checksum
+     for j=1:NB
+        e = calculateEnergyBand(Fs, ir_Win, Blo(j), Bhi(j))/length(ir_Win); %/(Bhi(j)-Blo(j)+1); 
+        E_BandWin(j,i,:) = e;
+        eSumBandsW= eSumBandsW+E_BandWin(j,i,:);
+     end
+     eSumBandsW= squeeze(eSumBandsW);
+     maxDistSL = maxDistSL+1;
+end
+
+%% -------
+%% BRIR Energy for each band
+E_BandBrir=zeros(NB,2);
+%eSumBands=zeros(1,1); %checksum
+eSumBands=0; %checksum
+for j=1:NB
+    %eSumBands = eSumBands+E_BandWin(j,i,:);
+    e = calculateEnergyBand(Fs, t_BRIR, Blo(j), Bhi(j))/length(t_BRIR);  %/(Bhi(j)-receiver.stopListening();Blo(j)+1);
+    E_BandBrir(j,:) = e;
+    eSumBands = eSumBands+E_BandBrir(j,:);
+end
+eSumBands= squeeze(eSumBands);
+%% --------------------------                    % FIGURE 1 -- Total: ISM, Windowed, BRIR-Windowed
+figure; hold on;                                 
+eL_Ism  = zeros(NumIRs,1);
+eL_Win  = zeros(NumIRs,1);
+eL_BRIR_W = zeros(NumIRs,1);
+%% Only one channel
+% eL_Ism = e_TotalIsm(:,L);   % Ism without direct path
+% eL_Win = e_TotalWin(:,L);   % Reverb files (hybrid windowed order 0 with no direct path)
+%% Average of both channels
+eL_Ism = (e_TotalIsm(:,L)+e_TotalIsm(:,R))./2;   % Ism without direct path
+eL_Win = (e_TotalWin(:,L)+e_TotalWin(:,R))./2;   % Reverb files (hybrid windowed order 0 with no direct path)
+%eL_Total=e_Total([1:1:length(e_Total)],1);      % TOTAL Ism+Rever sin camino directo
+
+plot (x, eL_Ism,'r--*');   % Ism
+plot (x, eL_Win,'g--o');   % Windowed
+%plot (x,eL_Total,'b--+'); % Total
+grid;
+
+eL_BRIR_W(:,1) = eBRIR_L*ones(length(NumIRs))-eL_Win;
+plot (x, eL_BRIR_W,'k--x');
+%ylim([0.0 0.8]);
+xlabel('Distance (m)');  
+ylabel('Energy'); 
+title('Total Energy vs Pruning Distance');  
+legend('E.Ism',  'E.win','E.RIR-E.win',  'Location','northwest');
+%% -----------------------------                 % FIGURE 2 -- Total Factor
+figure;                                          
+Factor = sqrt (eL_Ism ./ eL_BRIR_W);
+plot (x, Factor,'b--*');
+%% ylim([0.0 1.2]);
+
+FactorMeanValue=0;
+for j=DpMinFit:(DpMax-DpMin)+1
+    FactorMeanValue = FactorMeanValue+Factor(j,1);
+end
+FactorMeanValue = FactorMeanValue/(DpMax-DpMinFit);
+
+xlabel('Distance (m)');  
+ylabel('Factor'); 
+title('Factor (total) vs Pruning Distance');
+%% RGain = RGain_Linear*EnergyFactor;
+%RGain = FactorMeanValue*db2mag(RGain_dB); 
+formatLegendFactor= "Factor: %.3f -- RGain(dB): %.1f";
+legengFactor = sprintf(formatLegendFactor, FactorMeanValue, RGain_dB);  
+%legend('SQRT(eTotalIsm/(eRIR-eTotalwin))', 'Location','southeast');
+legend(legengFactor, 'Location','southeast');
+
+save ('EnergyFactor.mat','FactorMeanValue');
+
+grid;
+%% -----------------------------                 % FIGURE 3 -- Partial: ISM, Windowed, BRIR-Windowed
+figure; hold on;                                 
+for j=1:NB
+    subplot(NB,3,3*j-2);
+    %% Only one channel
+    % y=  E_BandIsm(j,:,L);
+    %% Average of both channels
+    y =(E_BandIsm(j,:,L)+ E_BandIsm(j,:,R))./2;
+    plot (x,y,'r--.');   %Ism
+    legend('e-BandIsm', 'Location','northwest');
+    ylim([0.0 0.1]);
+    %% ylim([0.0 0.01*j]);    grid;
+    
+    subplot(NB,3,3*j-1);
+    %% Only one channel
+    % y= E_BandWin(j,:,L);
+    %% Average of both channels
+    y =(E_BandWin(j,:,L)+ E_BandWin(j,:,R))./2;
+    plot (x,y,'g--.');   % Windowed
+    legend('e-BandWin', 'Location','northeast');
+    ylim([0.0 0.1]);
+    %% ylim([0.0 0.01*j]);    grid;
+
+    subplot(NB,3,3*j);
+    %% Only one channel
+    % eBand=E_BandBrir(j,L);
+    % y= E_BandWin(j,:,L);
+    %% Average of both channels
+    eBand=E_BandBrir(j,L);
+        y =E_BandWin(j,:,L);
+        E_BandBrir_Win(j,:,L)=abs(eBand(1,1)*ones(1, length(NumIRs))-y);
+        eBand=E_BandBrir(j,R);
+        y = E_BandWin(j,:,R);
+        E_BandBrir_Win(j,:,R)=abs(eBand(1,1)*ones(1, length(NumIRs))-y);
+        plot (x, (E_BandBrir_Win(j,:,L)+E_BandBrir_Win(j,:,R))./2);
+    legend('e-Rir-Win', 'Location','southeast');
+    ylim([0.0 0.1]);
+    %% ylim([0.0 0.01*j]);    grid;
+end
+% %% color map
+% c= [0.3333, 0.0 ,0.5; 0.6667, 0, 0.5; 1.0000, 0, 0.5; 1.0000, 0.3333, 0.5; 1.0000, 0.6667,0.5;
+%     1.0000, 0.5000, 0;  1.0000, 0.0000, 0.5000; 0.0000, 0.3333, 1.0000; 0.0000, 0.6667, 0.5000];
+% colormap(c);
+%% -----------------------------                  % FIGURE 4 -- Factor per Band
+figure; hold on;                                  
+factorBand =zeros(NB, NumIRs,2);
+for j=1:NB
+    %% Only one channel
+    % eBand=E_BandBrir(j,L);
+    % y= E_BandWin(j,:,L);
+    %% Average of both channels
+    eBand=E_BandBrir(j,L);
+        y =E_BandWin(j,:,L);
+        E_BandBrir_Win(j,:,L)=abs(eBand(1,1)*ones(1, length(NumIRs))-y);
+
+        eBand=E_BandBrir(j,R);
+        y =E_BandWin(j,:,R);
+        E_BandBrir_Win(j,:,R)=abs(eBand(1,1)*ones(1, length(NumIRs))-y);
+
+        E_BandIsm (j,:,L) = (E_BandIsm (j,:,L)+E_BandIsm (j,:,R))./2;
+        E_BandBrir_Win(j,:,L) = (E_BandBrir_Win(j,:,L)+E_BandBrir_Win(j,:,R))./2;
+        
+        factorBand(j,:,L) = sqrt(E_BandIsm (j,:,L) ./ E_BandBrir_Win(j,:,L));
+        plot (x, factorBand(j,:,L),"LineWidth",1.5);   % ,'color', [c(j,1) c(j,2) c(j,3)]
+end
+grid;
+%% ylim([0.0 3.5]);
+xlabel('Distance (m)');  ylabel('Factor'); 
+legend( 'B1','B2','B3','B4', 'B5','B6','B7','B8','B9','Location','northeast');
+title('Factor per Band vs Pruning Distance');  
+
+%% Curve Fitting                                   % FIGURE 5 -- Fit for each Band     
+xf=[DpMinFit:1:DpMax]; % from 10 meters to the end
+figure; hold on;                                     
+leg = {'B1', 'a1','B2', 'a2','B3','a3','B4','a4','B5', 'a5','B6','a6','B7','a7','B8','a8','B9','a9'};
+
+%fitObj= cfit.empty(0,NB); % Create empty array of specified class cfit
+%cfitData = struct(cfit);
+%cfitArray = repmat (cfitData, 1, NB);
+
+gof = struct([]);                                   % Create empty struct
+gofplus = struct('gof', gof , 'p1', 0, 'p2', 0);    % Create struct to load data per band
+gofpArray = repmat (gofplus, 1, NB);                % Array of structures to store information for each band
+
+for j=1:NB
+   Ff=factorBand(j, NumIRs-(DpMax-DpMinFit) : NumIRs, L);  % from 10 meters to the end
+   xft=xf'; Fft= Ff'; % transpose
+      % [fitObj, gof] = fit(xft,Fft,'poly1');
+   [fitObj, gofplus.gof] = fit(xft,Fft,'poly1');
+      % cfitArray(j) = struct(fitObj);
+   gofpArray(j).gof = gofplus.gof;
+   gofpArray(j).p1  = fitObj.p1;
+   gofpArray(j).p2  = fitObj.p2;
+      % disp(fitObj)  % disp(cfitArray(j)); 
+      % fitObj.p1;    % cfitArray(j).coeffValues(1,1);
+   p=plot(fitObj, xft,Fft, '--o');
+   p(2,1).Color = 'b'; p(1,1).LineWidth=1.5;
+end   
+%% ylim([0.0 3.5]);
+xlabel('Distance (m)');  ylabel('Factor'); 
+legend( leg, 'Location','northwest'); grid;
+title('CURVE FIT (9B)- Factor per Band vs Pruning Distance'); 
+%hold off;
---
+++ ISM_OSC_Tester/MatlabOscTester/View_IR_EDCAv.m
@@ -0,0 +1,198 @@
+%% This script shows the intermediate and final graphical results
+%% corresponding to applying the hybrid method (ISM+convolution) 
+
+% Author: Fabian Arrebola (19/10/2023) 
+% contact: areyes@uma.es
+% 3DDIANA research group. University of Malaga
+% Project: SONICOM
+% 
+% Copyright (C) 2023 Universidad de Málaga
+ 
+%% Set folder with IRs and Params
+cd 'C:\Repos\of_v0.11.2_vs2017_release\ImageSourceMethodTestApp\bin\data\resources\SeriesIr\12';
+%% cd 'C:\Repos\HIBRIDO PRUEBAS\New LAB 40 2 24\16'
+
+
+%% Load info
+load ("ParamsISM.mat");
+load ("FiInfAbsorb.mat");
+load ("FiInfSlopes.mat");
+load ("EnergyFactor.mat");
+
+%% Set working folder
+cd 'C:\Repos\of_v0.11.2_vs2017_release\ImageSourceMethodTestApp\bin\data\resources\SeriesIr';
+load ("ParamsHYB.mat");
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+formatFileISM= "iIrRO%iDP%02iW%02i";
+nameFileISM = sprintf(formatFileISM, RefOrd, Dp_Tmix, W_Slope)+'.wav';
+
+formatFileWin= "wIrRO0DP%02iW%02i";
+nameFileWin = sprintf(formatFileWin, Dp_Tmix, W_Slope)+'.wav';
+
+formatFileHyb= "tIrRO%iDP%02iW%02i";
+nameFileHyb = sprintf(formatFileHyb, RefOrd, Dp_Tmix, W_Slope)+'HYB.wav';
+
+formatFileBRIR= "wIrRO0DP01W02";
+nameFileBRIR = sprintf(formatFileBRIR)+'.wav';
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[yBRIR,Fs] = audioread(nameFileBRIR);
+[Ism,Fs] = audioread(nameFileISM);
+[Wind,Fs] = audioread(nameFileWin);
+[Hybrid,Fs] = audioread(nameFileHyb);
+%[HybridRT, Fs] = audioread (nameFileRT);
+
+ySum=Ism+Wind;
+%ySum=HybridRT;
+
+%yBRIR_2 = yBRIR.^2;
+yBRIR_2 = yBRIR.^2;
+Ism_2 = Ism.^2;
+Wind_2 = Wind.^2;
+ySum_2 = ySum.^2;
+Hybrid_2 = Hybrid.^2;
+
+% yBRIR_a = (yBRIR_2(:,1) + yBRIR_2(:,2))./2;
+% Ism_a = (Ism_2(:,1) + Ism_2(:,2))./2;
+% Wind_a = (Wind_2(:,1) + Wind_2(:,2))./2;
+% ySum_a = (ySum_2(:,1) + ySum_2(:,2))./2;
+% Hybrid_a = (Hybrid_2(:,1) + Hybrid_2(:,2))./2;
+
+EDCyBRIR  = zeros(length(yBRIR),2);
+EDCyBRIRdB= zeros(length(yBRIR),2);
+N= length(yBRIR);
+for I= 1:N
+   EDCyBRIR(I,1) = sum (yBRIR_2(I:N,1)) / (N);
+   EDCyBRIR(I,2) = sum (yBRIR_2(I:N,2)) / (N);
+end
+   
+EDCIsm= zeros(length(yBRIR),2);
+N= length(Ism);
+for I= 1:N
+   EDCIsm(I,1) = sum (Ism_2(I:N,1)) / (N);
+   EDCIsm(I,2) = sum (Ism_2(I:N,1)) / (N);
+end
+
+EDCWind= zeros(length(yBRIR),2);
+N= length(Wind);
+for I= 1:N
+   EDCWind(I,1) = sum (Wind_2(I:N,1)) / (N);
+   EDCWind(I,2) = sum (Wind_2(I:N,2)) / (N);
+end
+
+EDCySum= zeros(length(yBRIR),2);
+N= length(ySum);
+for I= 1:N
+   EDCySum(I,1) = sum (ySum_2(I:N,1)) / (N);
+   EDCySum(I,2) = sum (ySum_2(I:N,2)) / (N);
+end
+
+EDCHybrid= zeros(length(yBRIR),2);
+EDCHybriddB= zeros(length(yBRIR),2);
+N= length(Hybrid);
+for I= 1:N
+   EDCHybrid(I,1) = sum (Hybrid_2(I:N,1)) / (N);
+   EDCHybrid(I,2) = sum (Hybrid_2(I:N,2)) / (N);
+end
+
+subplot(2,5,1);
+plot (yBRIR(1:30000));
+ylim([-0.08 0.08]);
+title('BRIR.WAV');
+
+subplot(2,5,2);
+plot (Ism(1:30000));
+ylim([-0.08 0.08]);
+title('ISM');
+
+subplot(2,5,3);
+plot (Wind(1:30000));
+ylim([-0.08 0.08]);
+title('Wind');
+
+subplot(2,5,4);
+plot (ySum(1:30000));
+title('Sum');
+ylim([-0.08 0.08]);
+
+subplot(2,5,5);
+plot (Hybrid(1:30000));
+title('Hybrid');
+ylim([-0.08 0.08]);
+
+subplot(2,5,6);
+plot (EDCyBRIR(1:30000,1));
+plot (EDCyBRIR(1:30000,2));
+title('EDC-BRIR');
+%ylim([0.0 2e-5]);
+
+subplot(2,5,7);
+plot (EDCIsm(1:30000,1));
+plot (EDCIsm(1:30000,2));
+title('EDC-ISM');
+%ylim([0.0 2e-5]);
+
+subplot(2,5,8);
+plot (EDCWind(1:30000,1));
+plot (EDCWind(1:30000,2));
+title('EDC-WIND');
+%ylim([0.0 2e-5]);
+
+subplot(2,5,9);
+plot (EDCySum(1:30000,1));
+plot (EDCySum(1:30000,2));
+title('EDC-Sum');
+%ylim([0.0 2e-5]);
+
+subplot(2,5,10);
+plot (EDCHybrid(1:30000,1));
+plot (EDCHybrid(1:30000,2)); 
+title('EDCHybrid');
+%ylim([0.0 2e-5]);
+
+figure; 
+hold on;
+plot (EDCyBRIR(1:30000,1),'b');
+plot (EDCyBRIR(1:30000,2),'--b');
+
+plot (EDCHybrid(1:30000,1),'r');
+plot (EDCHybrid(1:30000,2),'--r');
+legend( 'BRIR_L','BRIR_R','Hybrid_L','Hybrid_R','Location','northeast');
+title('EDC BRIR vs Hybrid');
+ylabel('Energy decay');
+xlabel('Samples');
+%ylim([0.0 1e-4]);
+
+figure;
+hold on;
+plot (yBRIR(1:30000,1), 'b');
+plot (yBRIR(1:30000,2), '--b');
+
+plot (Hybrid(1:30000,1), 'r');
+plot (Hybrid(1:30000,2), '--r');
+legend( 'BRIR_L','BRIR_R','Hybrid_L','Hybrid_R','Location','northeast');
+% plot (ySum(1:30000));
+title('IRs: BRIR vs Hybrid');
+xlabel('Samples');
+hold off;
+
+figure;
+hold on;
+EDCyBRIRdB(:,1) = 10*log10(EDCyBRIR(:,1)./EDCyBRIR(1,1));
+plot(EDCyBRIRdB (1:44000,1), 'b');
+EDCyBRIRdB(:,2) = 10*log10(EDCyBRIR(:,2)./EDCyBRIR(1,2));
+plot(EDCyBRIRdB (1:44000,2), '--b');
+
+EDCHybriddB(:,1) = 10*log10(EDCHybrid(:,1)./EDCHybrid(1,1));
+plot(EDCHybriddB(1:44000,1), 'r');
+EDCHybriddB(:,2) = 10*log10(EDCHybrid(:,2)./EDCHybrid(1,2));
+plot(EDCHybriddB(1:44000,2), '--r');
+
+legend( 'BRIR_L','BRIR_R','Hybrid_L','Hybrid_R','Location','northeast');
+%EDCySumdB = 10*log10(EDCySum./EDCySum(1));
+%plot(EDCySumdB(1:44000), 'k');
+
+ylabel('Energy decay [dB]');
+xlabel('Samples');
+title('EDC BRIR vs Hybrid');
